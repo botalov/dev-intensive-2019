c86234cff13a3b563b9722e97c7eb189fd588529
botalov <botalov.n@carrotquest.io>
2019-06-30 13:45:46 +0500

    Добавлена функция-расщирение Date.add

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,7 +1,9 @@
 package ru.skillbranch.devintensive.extensions
 
+import java.lang.IllegalStateException
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 const val SECOND = 1000L
 const val MINUTE = 60 * SECOND
@@ -11,5 +13,20 @@ const val DAY = 24 * HOUR
 fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy") : String{
     val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
     return dateFormat.format(this)
+}
+
+
+fun Date.add(value: Int, units: TimeUnit = TimeUnit.SECONDS): Date {
+    var time = this.time
+
+    time += when(units) {
+        TimeUnit.SECONDS -> value * SECOND
+        TimeUnit.MINUTES -> value * MINUTE
+        TimeUnit.HOURS -> value * HOUR
+        TimeUnit.DAYS -> value * DAY
+        else -> throw IllegalStateException("invalid units")
+    }
+    this.time = time
+    return this
 }
 
