b3d09eee59bda49f57238cd981cdd422fe11564e
botalov <botalov.n@carrotquest.io>
2019-07-03 08:28:27 +0500

    *Builder

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -40,7 +40,7 @@ class User (
     }
 
     data class Builder (
-            var id: String? = null,
+            var id: String = "",
             var firstName: String? = null,
             var lastName: String? = null,
             var avatar: String? = null,
@@ -51,26 +51,20 @@ class User (
 
         fun id(id: String) = apply { this.id = id }
 
-        fun firstName(firstName: String) = apply {this.firstName = firstName}
+        fun firstName(firstName: String?) = apply {this.firstName = firstName}
 
-        fun lastName(lastName: String) = apply { this.lastName = lastName }
+        fun lastName(lastName: String?) = apply { this.lastName = lastName }
 
-        fun avatar(avatar: String) = apply { this.avatar = avatar }
+        fun avatar(avatar: String?) = apply { this.avatar = avatar }
 
         fun rating(rating: Int) = apply { this.rating = rating }
 
         fun respect(respect: Int) = apply { this.respect = respect }
 
-        fun lastVisit(lastVisit: Date) = apply { this.lastVisit = lastVisit }
+        fun lastVisit(lastVisit: Date?) = apply { this.lastVisit = lastVisit }
 
         fun isOnline(isOnline: Boolean) = apply { this.isOnline = isOnline }
 
-
-        fun build(): User {
-            if (id.isNullOrEmpty()) {
-                throw Exception("id is null")
-            }
-            return User(id!!, firstName, lastName, avatar, rating, respect, lastVisit, isOnline)
-        }
+        fun build(): User = User(id, firstName, lastName, avatar, rating, respect, lastVisit, isOnline)
     }
 }
