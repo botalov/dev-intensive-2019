2c0c833cb2869f75a6190544ff3e660d26419d09
Nikita <botalovnikita@gmail.com>
2019-07-20 17:42:57 +0500

    isKeyboardOpen, isKeyboardClosed

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -1,9 +1,13 @@
 package ru.skillbranch.devintensive.extensions
 
 import android.app.Activity
+import android.content.Context
 import android.content.Context.INPUT_METHOD_SERVICE
+import android.graphics.Rect
+import android.util.TypedValue
+import android.view.View
 import android.view.inputmethod.InputMethodManager
-
+import kotlin.math.roundToInt
 
 
 fun Activity.hideKeyboard() {
@@ -12,4 +16,22 @@ fun Activity.hideKeyboard() {
         val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
+}
+
+fun Activity.isKeyboardOpen(): Boolean {
+    val visibleBounds = Rect()
+    this.findViewById<View>(android.R.id.content).getWindowVisibleDisplayFrame(visibleBounds)
+    val heightDiff = this.findViewById<View>(android.R.id.content).height - visibleBounds.height()
+    val marginOfError = this.convertDpToPx(50F).roundToInt()
+    return heightDiff > marginOfError
+}
+fun Activity.isKeyboardClosed(): Boolean = !this.isKeyboardOpen()
+
+
+private fun Context.convertDpToPx(dp: Float): Float {
+    return TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        dp,
+        this.resources.displayMetrics
+    )
 }
